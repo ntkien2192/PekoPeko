d1d56f1469ea39c0864937a99d62d4a4c1711a44
Hieu Tran <658655@gmail.com>
2016-10-13 10:11:35 +0700

    Remove Pods folder to save repo storage

--- Pods/Target Support Files/Pods-PekoPekoUITests/Pods-PekoPekoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PekoPekoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PekoPekoUITests
-@end
--- Pods/Target Support Files/UIResponder+KeyboardCache/UIResponder+KeyboardCache-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_UIResponder_KeyboardCache : NSObject
-@end
-@implementation PodsDummy_UIResponder_KeyboardCache
-@end
